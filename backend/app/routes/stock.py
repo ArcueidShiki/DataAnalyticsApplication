319488d3bf12b1374861f7c20dd45e104a33039f
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2025-05-09 14:37:58 +0800

    Fix CRSF token headers bug in preflight request
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- backend/app/routes/stock.py
+++ backend/app/routes/stock.py
@@ -55,7 +55,7 @@ def add_cors_headers(response):
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Credentials"] = "true"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-CSRF-Token"
     return response
 
 @stock_bp.route('/watchlist', methods=['GET', 'POST', 'OPTIONS'])
